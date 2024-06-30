a817062d55fca7e39dc41ae74e4dd4003abe7898
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-06-30 20:48:35 +0600

    super quality is done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-const Button = ({ label, iconUrl }) => {
+const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img src={iconUrl} className="ml-2 rounded-full size-5" alt="" />
+      <img src={iconURL} className="ml-2 rounded-full size-5" alt="" />
     </button>
   );
 };
